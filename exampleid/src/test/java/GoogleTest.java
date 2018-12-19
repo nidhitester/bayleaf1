059fd4e29eca787a17d1d55a6904d27a4b7ad3bd
nidhitester <nidhibayleaf@gmail.com>
2018-12-19 14:05:55 +0000

    COde

---
+++ exampleid/src/test/java/GoogleTest.java
@@ -0,0 +1,22 @@
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class GoogleTest {
+    private static WebDriver driver;
+    @Test
+    public void google(){
+
+        System.setProperty("webdriver.chrome.driver","\\Soft\\chromedriver.exe");
+        driver=new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.MILLISECONDS);
+        driver.manage().window().maximize();
+        driver.get("https://www.google.com/");
+        driver.findElement(By.xpath("//input[@title=\"Search\"]")).sendKeys("food");
+        driver.findElement(By.name("btnK")).click();
+
+    }
+}
